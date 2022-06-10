d63d72a872ccb873cfc6d0ce0a28130c14dbbb1d
liwh <1171436478@qq.com>
2022-06-10 09:48:30 +0000

    rebuild 2

--- ops-engine/setup_env.sh
+++ ops-engine/setup_env.sh
@@ -1,7 +1,7 @@
 # this script runs on ubuntu
 #!bash
 apt-get update && apt-get install -y locales
-apt install -y make git unzip
+apt install -y make git unzip gcc
 mkdir /home/dev && cd /home/dev
 git clone https://github.com/wg/wrk.git
 cd wrk && make
@@ -10,10 +10,12 @@ apt-get install -y python3 python3-pip
 pip install fastapi[all]
 apt-get install -y inetutils-ping
 pip install fabric
-apt install iperf3 nmap
-apt install sudo curl
+apt install -y iperf3 nmap
+apt install -y sudo curl
 
 
 # nodejs
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-apt-get install nodejs npm
+apt-get install nodejs 
+sudo apt-get update && sudo apt-get install yarn
+apt-get install -y npm
