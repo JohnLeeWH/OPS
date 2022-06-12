6468369b0d7d93f8ed5343aa68df0e2c892078a6
leewh <1171436478@qq.com>
2022-06-12 15:38:25 +0000

    welcome compo test

--- ops-page/src/App.js
+++ ops-page/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import React from "react";
 import Test from "./components/Great";
+import Welcome from "./components/Welcome";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Test />
+        <Welcome />
       </div>
     )
   }
