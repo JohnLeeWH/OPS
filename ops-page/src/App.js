34d05279b90cf35b10a8ffa513fa9706bf619b2b
liwh <1171436478@qq.com>
2022-06-14 10:28:11 +0000

    test state1

--- ops-page/src/App.js
+++ ops-page/src/App.js
@@ -3,16 +3,12 @@ import React from "react";
 import Test from "./components/Great";
 import Welcome from "./components/Welcome";
 import Hello from "./components/hello";
+import Message from "./components/Message";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Test name="123" nickName="一" >tttt</Test>
-        <Test name="234" nickName="二" />
-        <Test name="345" nickName="三" />
-        <Welcome name="123" nickName="一" >www</Welcome>
-      </div>
+      <Message />
     )
   }
 }
---
+++ ops-page/src/components/Message.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Message extends Component {
+    render() {
+        return <h1>message !</h1>;
+    }
+};
+
+export default Message;
