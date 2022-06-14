a4eeb3138c521dea8f17f2941ec55a6672b87f09
leewh <1171436478@qq.com>
2022-06-14 16:46:19 +0000

    test state 3

--- ops-page/src/components/Message.js
+++ ops-page/src/components/Message.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 
 class Message extends Component {
-    render() {
-        return <h1>message !</h1>;
+    constructor() {
+        super();
+        this.state = {
+            message: "欢迎！",
+        };
     }
+    changeMessage() {
+        this.setState({ message: "click!" });
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick={() => this.changeMessage()}>subscribe</button>
+            </div >
+        );
+    };
 };
 
 export default Message;
