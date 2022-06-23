6214e6929d2704d4e3b62ce25ff25e9e4137c081
liwh <1171436478@qq.com>
2022-06-23 15:50:24 +0000

    child parent props

--- ops-page/src/App.js
+++ ops-page/src/App.js
@@ -9,12 +9,13 @@ import Great from "./components/Great";
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
+import ParentComponent from "./components/parentComponent";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <EventBind />
+        <ParentComponent />
       </div>
     )
   }
