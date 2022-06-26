0793d1aa857895a05f4ddf0a3241e13e43c5b597
leewh <1171436478@qq.com>
2022-06-26 17:14:27 +0000

    if else

---
+++ ops-page/src/components/UserGreeting.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Hello from "./hello";
+
+class UserGreeting extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: true,
+        };
+    }
+    render() {
+        return this.state.isLoggedIn ? (<div>Hello user</div>) : (<div>hello guest</div>)
+        // let message;
+        // if (this.state.isLoggedIn) {
+        //     message = <div>hello user</div>
+        // } else {
+        //     message = <div>hello guest</div>
+        // }
+        // return <div>{message}</div>
+        // if (this.state.isLoggedIn) {
+        //     return <div>hello user</div>
+        // } else {
+        //     return <div>hello guest</div>
+        // }
+    }
+}
+
+export default UserGreeting;
