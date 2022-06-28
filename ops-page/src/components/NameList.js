f633aeab1bf82961b102de7906a43e064a32d534
liwh <1171436478@qq.com>
2022-06-28 06:42:01 +0000

    list map

---
+++ ops-page/src/components/NameList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Person from './Person';
+
+function NameList() {
+    const persons = [
+        {
+            id: 1,
+            name: "John",
+            age: 11,
+            skill: "react",
+        },
+        {
+            id: 2,
+            name: "Bob",
+            age: 12,
+            skill: "js",
+        },
+        {
+            id: 3,
+            name: "Peter",
+            age: 13,
+            skill: "python",
+        },
+    ]
+    const personList = persons.map((person) => (
+        <Person person={person} />
+    ));
+    return (
+        <div>
+            {personList}
+        </div>
+    );
+}
+
+export default NameList;
