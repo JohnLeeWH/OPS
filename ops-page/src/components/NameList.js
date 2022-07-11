b072422ac4518a9b33a9000755abbe891399e54d
leewh <1171436478@qq.com>
2022-07-11 16:58:36 +0000

    index as key

--- ops-page/src/components/NameList.js
+++ ops-page/src/components/NameList.js
@@ -2,32 +2,12 @@ import React from 'react'
 import Person from './Person';
 
 function NameList() {
-    const persons = [
-        {
-            id: 1,
-            name: "John",
-            age: 11,
-            skill: "react",
-        },
-        {
-            id: 2,
-            name: "Bob",
-            age: 12,
-            skill: "js",
-        },
-        {
-            id: 3,
-            name: "Peter",
-            age: 13,
-            skill: "python",
-        },
-    ]
-    const personList = persons.map((person) => (
-        <Person key={person.id} person={person} />
-    ));
+    const names = ["John", "Bob", "Peter","John "];
+    const nameList = names.map((name, index) => (<h2 key={index}>{index}:{name}</h2>))
+
     return (
         <div>
-            {personList}
+            {nameList}
         </div>
     );
 }
