89471749690be07409b2e4b69e2ef403cbca12f9
JohnLee <39941726+JohnLeeWH@users.noreply.github.com>
2022-07-31 23:17:01 +0800

    Update setup_env.sh
    update apt to apt-get

--- ops-engine/setup_env.sh
+++ ops-engine/setup_env.sh
@@ -1,7 +1,7 @@
 # this script runs on ubuntu
 #!bash
 apt-get update && apt-get install -y locales
-apt install -y make git unzip gcc
+apt-get install -y make git unzip gcc
 mkdir /home/dev && cd /home/dev
 git clone https://github.com/wg/wrk.git
 cd wrk && make
@@ -10,8 +10,8 @@ apt-get install -y python3 python3-pip
 pip install fastapi[all]
 apt-get install -y inetutils-ping
 pip install fabric
-apt install -y iperf3 nmap
-apt install -y sudo curl
+apt-get install -y iperf3 nmap
+apt-get install -y sudo curl
 
 
 # nodejs
